3c987ae469e10bb644d16e2bd0b7e07e1692146d
khushi Khandelwal _ poornima Institute of Engineering and Technology <khushi.khandelwal@celebaltech.com>
2023-08-22 17:40:43 +0530

    First Commit

---
+++ Operators/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arithmetic operators
+            //int result;
+            //int x = 20, y = 8;
+
+            //result = (x + y);
+            //Console.WriteLine("Addition:" + result);
+
+            //result = (x - y);
+            //Console.WriteLine("Subtraction:" + result);
+
+            //result = (x * y);
+            //Console.WriteLine("Multiplication: " + result);
+
+            //result = (x / y);
+            //Console.WriteLine("Division: " + result);
+
+            //result = (x % y);
+            //Console.WriteLine("Modulo: " + result);
+
+            
+            //Unary operators
+            //int a = 1, result;
+            //result = ((a++) + (++a));
+            //Console.WriteLine(result);
+
+            //result = a++;
+            //Console.WriteLine("a is {0} and result is {1}", a, result);
+            //result = a--;
+            //Console.WriteLine("a is {0} and result is {1}", a, result);
+            //result = ++a;
+            //Console.WriteLine("a is {0} and result is {1}", a, result);
+            //result = --a;
+            //Console.WriteLine("a is {0} and result is {1}", a, result);
+
+
+            //Relational operators
+
+            //bool result;
+            //int x = 15, y = 10;
+            //result = (x == y);
+            //Console.WriteLine("Equal to Op: " + result);
+            //result = (x > y);
+            //Console.WriteLine("Greater than op: " + result);
+            //result = (x < y);
+            //Console.WriteLine("Less than Op: " + result);
+            //result = (x >= y);
+            //Console.WriteLine("Greater than or Equal to: " + result);
+            //result = (x <= y);
+            //Console.WriteLine("Lesser than or Equal to: " + result);
+            //result = (x != y);
+            //Console.WriteLine("Not Equal to Op: " + result);
+
+
+
+            //Assignment Operators
+            //int x = 19;
+            //x += 5;
+            //Console.WriteLine("Add Assignment Operator: " + x);
+            //x = 10;
+            //x -= 5;
+            //Console.WriteLine("Subtract Assignment Operator: " + x);
+            //x = 15;
+            //x *= 5;
+            //Console.WriteLine("Multiply Assignment Operator: " + x);
+            //x = 30;
+            //x /= 5;
+            //Console.WriteLine("Division Assignment Operator: " + x);
+            //x = 10;
+            //x %= 5;
+            //Console.WriteLine("Modulo Assignment Operator: " + x);
+
+
+
+
+            ////Conditinal operators
+            //int x = 28, y = 28, result;
+            //result = x > y ? x : y;
+            //Console.WriteLine("Result: " + result);
+            //result = x < y ? x : y;
+            //Console.WriteLine("Result: " + result);
+
+
+
+
+            ////Logical operators
+            //bool a = true, b = false, result;
+            //result = a && b;
+            //Console.WriteLine("AND Op: " + result);
+            //result = a || b;
+            //Console.WriteLine("OR Op: " + result);
+            //result = !b;
+            //Console.WriteLine("NOT Op: " + result);
+
+
+            //Bitwise Operators
+            //int x = 2, y = 10, result;
+            //result = x & y;
+            //Console.WriteLine("Bitwise AND: " + result);
+            //result = x | y;
+            //Console.WriteLine("Bitwise OR: " + result);
+            //result = x ^ y;
+            //Console.WriteLine("Bitwise XOR: " + result);
+            //result = ~x;
+            //Console.WriteLine("Bitwise Complement: " + result);
+            //result = x << 2;
+            //Console.WriteLine("Bitwise Left Shift: " + result);
+            //result = x >> 2;
+            //Console.WriteLine("Bitwise Right Shift: " + result);
+
+
+
+        }
+    }
+}
